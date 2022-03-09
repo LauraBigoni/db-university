27607719dba603ac8763d458a41d0ad05e9bcc0c
Laura Bigoni <laurabigoni.92@gmail.com>
2022-03-09 15:18:07 +0100

    ex.2

--- university_db.sql
+++ university_db.sql
@@ -3,7 +3,17 @@ SELECT `surname`, `name`, `date_of_birth` AS 'anno_di_nascita'
 FROM `students`
 WHERE YEAR(`date_of_birth`) = '1990';
 
-SELECT COUNT(*) FROM `students` WHERE YEAR(`date_of_birth`) = '1990';
+SELECT COUNT(*) 
+FROM `students` 
+WHERE YEAR(`date_of_birth`) = '1990';
 -- COUNT(*) 160
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
+SELECT `cfu`, `name` AS 'cfu_over_10' 
+FROM `courses` 
+WHERE `cfu` > 10;
+
+SELECT COUNT(*) 
+FROM `courses` 
+WHERE `cfu` > 10;
+-- COUNT(*) 479
