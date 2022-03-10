403ae8890d0ddda9493ca2ead807ee77a8213ca6
Laura Bigoni <laurabigoni.92@gmail.com>
2022-03-10 16:32:10 +0100

    added ex.7 and correction ex.6

--- join_university.sql
+++ join_university.sql
@@ -6,7 +6,6 @@ FROM `students`
 JOIN `degrees`
 ON `students`.`degree_id` = `degrees`.`department_id`
 HAVING `degrees`.`name` = 'Corso di Laurea in Economia';
---  76 total
 
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
@@ -15,7 +14,6 @@ FROM `degrees`
 JOIN `departments`
 ON `departments`.`id`= `degrees`.`department_id`
 WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
--- 7 total
 
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
@@ -27,7 +25,6 @@ JOIN `teachers`
 ON `course_teacher`.`teacher_id` = `teachers`.`id`
 WHERE `teachers`.`name` = 'Fulvio'
 AND `teachers`.`surname` = 'Amato';
--- 11 total
 
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
@@ -37,16 +34,38 @@ JOIN `degrees`
 ON `students`.`degree_id` = `degrees`.`id`
 JOIN `departments`
 ON `departments`.`id` = `degrees`.`department_id`
-ORDER BY `students`.`surname` , `students`.`name`
--- 5000 total
+ORDER BY `students`.`surname` , `students`.`name`;
 
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
-
+SELECT *
+FROM `degrees`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`id` 
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `course_teacher`.`course_id` = `teachers`.`id`;
 
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
-
+SELECT `teachers`.*, `departments`.`name` 
+FROM `departments` 
+JOIN `degrees` 
+ON `departments`.`id` = `degrees`.`department_id` 
+JOIN `courses` 
+ON `degrees`.`id` = `courses`.`degree_id` 
+JOIN `course_teacher` 
+ON `courses`.`id` = `course_teacher`.`course_id` 
+JOIN `teachers` 
+ON `teachers`.`id` = `course_teacher`.`teacher_id` 
+WHERE `departments`.`name` = 'Dipartimento di Matematica';
 
 
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+SELECT `students`.`surname`, `students`.`name`, COUNT(`exam_student`.`vote`) AS `attempts`
+FROM `exam_student` 
+JOIN `students`
+ON `exam_student`.`student_id` = `students`.`id`
+WHERE `exam_student`.`vote` < 18
+GROUP BY `students`.`id`;
