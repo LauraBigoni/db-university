acf9782aef5ee1419fccb873cf68809dddbb7795
Laura Bigoni <laurabigoni.92@gmail.com>
2022-03-10 15:22:34 +0100

    ex.2

--- join_university.sql
+++ join_university.sql
@@ -1,21 +1,36 @@
 ---------------- JOIN ----------------
 
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT `students`.`surname`, `students`.`name`,  `degrees`.`name`
+FROM `students`
+JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`department_id`
+HAVING `degrees`.`name` = 'Corso di Laurea in Economia';
+--  76 total
 
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT `degrees`.`name`, `departments`.`name`
+FROM `degrees`
+JOIN `departments`
+ON `departments`.`id`= `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
 
+
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
 
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
 
+
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
 
 
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
