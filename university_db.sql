1d68ec097cabaabc8734ad1dd94351e174685448
Laura Bigoni <laurabigoni.92@gmail.com>
2022-03-10 15:10:39 +0100

    added new exercise file

--- university_db.sql
+++ university_db.sql
@@ -23,7 +23,7 @@ WHERE `period` = 'i semestre' AND `year` = 1;
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 SELECT `date`, `hour`
 FROM `exams`
-WHERE `date` = '2020-06-20' AND `hour` >= '14:00:00';
+WHERE `date` = '2020-06-20' AND HOUR(`hour`) >= 14;
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 SELECT `name`, `level`
@@ -32,10 +32,10 @@ WHERE `level` = 'magistrale';
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 SELECT COUNT(*)
-FROM `departments`
+FROM `departments`;
 
 --8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
-SELECT `surname`, `name`, `phone`
+SELECT COUNT(*)
 FROM `teachers`
 WHERE `phone` IS NULL;
 
@@ -45,15 +45,16 @@ WHERE `phone` IS NULL;
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 SELECT COUNT(*) AS `students`, YEAR(`enrolment_date`) AS 'year'
 FROM `students`  
-GROUP BY YEAR(`enrolment_date`);
+GROUP BY `year`;
 
 --2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 SELECT COUNT(*) AS `teachers`, `office_address` AS 'same address'
 FROM `teachers`  
 GROUP BY `office_address`
+HAVING `teachers` > 1;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-SELECT `exam_id`, AVG(`vote`) AS 'avarage_votes'
+SELECT `exam_id`, ROUND(AVG(`vote`)) AS 'avarage_votes'
 FROM `exam_student`  
 GROUP BY `exam_id`;
 
